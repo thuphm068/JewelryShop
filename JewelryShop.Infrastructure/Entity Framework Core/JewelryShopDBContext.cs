8b69e6a529d0387dec3530465854959572cfe9ac
Thư Phạm <thu0349554192@outlook.com>
2023-04-24 14:42:12 +0700

    set up dbcontext and migrations

--- JewelryShop.Infrastructure/Entity Framework Core/JewelryShopDBContext.cs
+++ JewelryShop.Infrastructure/Entity Framework Core/JewelryShopDBContext.cs
@@ -6,67 +6,134 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using JewelryShop.Domain.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
 
 
-namespace JewelryShop.Infrastructure.Entity_Framework_Core
+namespace JewelryShop.Infrastructure.Entity_Framework_Core;
+
+public class JewelryShopDBContext : DbContext
 {
-    public class JewelryShopDBContext:DbContext
+    public DbSet<Product> _products { get; set; } = null;
+    public DbSet<Customer> _customers { get; set; } = null;
+    public DbSet<Cart> _carts { get; set; } = null;
+    public DbSet<CartItem> _cartitems { get; set; } = null;
+    public DbSet<Order> _orders { get; set; } = null;
+    public DbSet<OrderDetail> _orderdetails { get; set; } = null;
+    public DbSet<Category> _categories { get; set; } = null;
+    public DbSet<ProductCategory> _productcategories { get; set; } = null;
+
+    public DbSet<Warranty> _warrantys { get; set; } = null;
+
+    public JewelryShopDBContext(DbContextOptions<JewelryShopDBContext> options) : base(options)
     {
-        public DbSet<Product> _products { get; set; } = null;
-        public DbSet<Customer> _customers { get; set; } = null;
-        public DbSet<Cart> _carts { get; set; } = null;
-        public DbSet<CartItem> _cartitems { get; set; } = null;
-        public DbSet<Order> _orders { get; set; } = null;
-        public DbSet<OrderDetail> _orderdetails { get; set; } = null;
-        public DbSet<Category> _categories { get; set; } = null;
-        public DbSet<Warranty> _warrantys { get; set; } = null;
-
-        public JewelryShopDBContext(DbContextOptions<JewelryShopDBContext> options) : base(options)
-        {
-
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-            modelBuilder.Entity<Product>(re =>
-            {
-                re.ToTable("Product");
-                re.HasKey(r => r.Id);
-                re.Property(r => r.Name).IsRequired().HasMaxLength(128);
-                re.Property(r => r.Price).IsRequired();
-                re.Property(r => r.Description).IsRequired().HasMaxLength(5000);
-                re.HasOne<Warranty>().WithOne().HasForeignKey<Warranty>(x => x.Id).IsRequired();                //re.Property(r => r.WarrantyId);
-                re.Property(r => r.CategoryId).IsRequired();
-
-            });
-            modelBuilder.Entity<Customer>(re =>
-            {
-                re.ToTable("Customer");
-                re.HasKey(r => r.Id);
-                re.Property(r => r.Name).IsRequired();
-                re.Property(r => r.Address).IsRequired().HasMaxLength(300);
-                re.Property(r => r.Email).IsRequired().HasMaxLength(320);
-                re.Property(r => r.Password).IsRequired();
-                re.Property(r => r.Gender).IsRequired();
-                re.Property(r => r.Birthday);
-                re.Property(r => r.Phone).IsRequired();
-            });
-
-            modelBuilder.Entity<Cart>()
-                .ToTable("Cart")
-                .HasKey(r => r.Id);
-
-            modelBuilder.Entity<CartItem>()
-                .ToTable("CartItem")
-                .HasKey(r => r.Id);
-            modelBuilder.Entity<Order>()
-                .ToTable("Order")
-                .HasKey(r => r.Id);
-            modelBuilder.Entity<OrderDetail>().ToTable("OrderDetail");
-            modelBuilder.Entity<Category>().ToTable("Category");
-
-        }
 
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Warranty>(re =>
+        {
+            re.ToTable("Warranty");
+            re.HasKey(x => x.Id);
+            re.Property(r => r.Period).IsRequired();
+            re.Property(r => r.Description).IsRequired();
+        });
+
+        modelBuilder.Entity<Product>(re =>
+        {
+            re.ToTable("Product");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.Name).IsRequired().HasMaxLength(100);
+            re.Property(r => r.Price).IsRequired();
+            re.Property(r => r.Description).IsRequired();
+            re.Property(r => r.Material).IsRequired().HasMaxLength(20);
+            re.HasOne<Warranty>().WithMany().HasForeignKey(x => x.Id).IsRequired(false);
+        });
+        modelBuilder.Entity<Customer>(re =>
+        {
+            re.ToTable("Customer");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.Name).IsRequired().HasMaxLength(50);
+            re.Property(r => r.Address).IsRequired().HasMaxLength(200);
+            re.Property(r => r.Email).IsRequired().HasMaxLength(50);
+            re.Property(r => r.Password).IsRequired().HasMaxLength(20);
+            re.Property(r => r.Gender).IsRequired();
+            re.Property(r => r.Birthday).IsRequired();
+            re.Property(r => r.Phone).IsRequired().HasMaxLength(10);
+        });
+
+        modelBuilder.Entity<Cart>(re =>
+        {
+            re.ToTable("Cart");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.Total).IsRequired();
+            re.HasOne<Customer>().WithOne().HasForeignKey<Cart>(r => r.Id).IsRequired();//test
+        });
+
+
+
+        modelBuilder.Entity<CartItem>(re =>
+        {
+            re.ToTable("CartItem");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.TotalPrice).IsRequired();
+            re.Property(r => r.Quantity).IsRequired();
+            re.HasOne<Cart>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+            re.HasOne<Product>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+
+
+        });
+
+        modelBuilder.Entity<Order>(re =>
+        {
+            re.ToTable("Order");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.Status).IsRequired();
+            re.Property(r => r.Total).IsRequired();
+            re.Property(r => r.Date).IsRequired();
+            re.HasOne<Customer>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+        });
+
+
+        modelBuilder.Entity<OrderDetail>(re =>
+        {
+            re.ToTable("OrderDetail");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.Quantity).IsRequired();
+            re.Property(r => r.TotalPrice).IsRequired();
+            re.HasOne<Order>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+            re.HasOne<Product>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+        });
+        modelBuilder.Entity<Category>(re =>
+        {
+            re.ToTable("Category");
+            re.HasKey(r => r.Id);
+            re.Property(r => r.Name).IsRequired().HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<ProductCategory>(re =>
+        {
+            re.ToTable("ProductCategory");
+            re.HasKey(r => r.Id);
+            re.HasOne<Product>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+            re.HasOne<Category>().WithMany().HasForeignKey(x => x.Id).IsRequired();
+
+        });
+
+
+    }
+
+
+
+}
+
+public class JewelryShopContextFactory : IDesignTimeDbContextFactory<JewelryShopDBContext>
+{
+public JewelryShopDBContext CreateDbContext(string[] args)
+{
+    var optionsBuilder = new DbContextOptionsBuilder<JewelryShopDBContext>();
+    optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=JewelryShop;Trusted_Connection=True;TrustServerCertificate=True");
+    return new JewelryShopDBContext(optionsBuilder.Options);
+}
 }
---
+++ JewelryShop.Infrastructure/Migrations/20230424063624_first_migration.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelryShop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class first_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Warranty",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Period = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warranty", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Total = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cart_Customer_Id",
+                        column: x => x.Id,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Total = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Customer_Id",
+                        column: x => x.Id,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WarrantyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Material = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Warranty_Id",
+                        column: x => x.Id,
+                        principalTable: "Warranty",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CartId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartItem_Cart_Id",
+                        column: x => x.Id,
+                        principalTable: "Cart",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CartItem_Product_Id",
+                        column: x => x.Id,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetail",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetail", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Order_Id",
+                        column: x => x.Id,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetail_Product_Id",
+                        column: x => x.Id,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductCategory_Category_Id",
+                        column: x => x.Id,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductCategory_Product_Id",
+                        column: x => x.Id,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItem");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetail");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategory");
+
+            migrationBuilder.DropTable(
+                name: "Cart");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Warranty");
+        }
+    }
+}
